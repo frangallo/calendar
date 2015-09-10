0162d38ab0c8259b7a654d82e5ffd889e0446af5
frangallo <fgallo@me.com>
2015-09-10 11:16:19 -0700

    Styled modal

--- js/calendar.js
+++ js/calendar.js
@@ -10,17 +10,57 @@
   }
 
   CalendarView.prototype.openModal = function (){
-    days = [1,7,14,24]
+    var that = this;
     $(".indv-days td").click(function(event){
       $day = $(event.currentTarget);
       dayInfo = $day.attr("data-info");
-      console.log(dayInfo === "1");
-      if (dayInfo === "1"){
-        console.log("hello")
-        $('body').append("<div>").addClass('m-background')
-        $(".m-content").show();
+      if (dayInfo === "2"){
+        that.openFirstModal();
+      } else if (dayInfo === "13") {
+        that.openSecondModal();
+      } else if (dayInfo === "24") {
+        that.openThirdModal();
       }
     })
+  }
+
+  CalendarView.prototype.openFirstModal = function (){
+    console.log("hello")
+    $('body').append("<div>").addClass('m-background')
+    $(".date").html("September 1, 2015")
+    $(".event").html("Birthday Party")
+    $(".invite-count").html("Invite Count: 120")
+    $(".circle").addClass("circle-green")
+    $(".m-content").show();
+    $(".close-button").click(function(){
+      location.reload()
+    });
+  }
+
+  CalendarView.prototype.openSecondModal = function (){
+    console.log("hello")
+    $('body').append("<div>").addClass('m-background')
+    $(".date").html("September 13, 2015")
+    $(".event").html("Technical Discussion")
+    $(".invite-count").html("Invite Count: 23")
+    $(".circle").addClass("circle-green")
+    $(".m-content").show();
+    $(".close-button").click(function(){
+      location.reload()
+    });
+  }
+
+  CalendarView.prototype.openThirdModal = function (){
+    console.log("hello")
+    $('body').append("<div>").addClass('m-background')
+    $(".date").html("September 24, 2015")
+    $(".event").html("Press Release")
+    $(".invite-count").html("Invite Count: 64")
+    $(".circle").addClass("circle-red")
+    $(".m-content").show();
+    $(".close-button").click(function(){
+      location.reload()
+    });
   }
 
   CalendarView.prototype.buildDays = function(){
@@ -37,7 +77,12 @@
       } else if (i === 29){
         $(".indv-days").append($("<tr></tr>"));
       }
-      $(".indv-days").append($("<td></td>").text(i).attr('data-info', i));
+
+      if ( i === 2 || i === 13 || i === 24){
+        $(".indv-days").append($("<td></td>").addClass("busy-day").text(i).attr('data-info', i));
+      } else {
+        $(".indv-days").append($("<td></td>").text(i).attr('data-info', i));
+      }
     }
   }
 
